ee91a6b98f5e1493411facedef123e9da48fd9a8
HanniAungMyo <hsuhanni095@gmail.com>
2025-02-25 20:30:39 +0630

    feat: HTTPClient [Read]

---
+++ HAMDotNetCore.ConsoleApp/HTTPClientExample/HttpClientExample.cs
@@ -0,0 +1,40 @@
+﻿using HAMDotNetCore.ConsoleApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAMDotNetCore.ConsoleApp.HTTPClientExample
+{
+    public class HttpClientExample
+    {
+        public async Task
+             Run()
+        {
+            await Read();
+        }
+        private async Task Read()
+        {
+            HttpClient client=new HttpClient();
+            HttpResponseMessage response= await client.GetAsync("https://localhost:7160/api/blogs");
+            if (response.IsSuccessStatusCode) 
+            {
+             string json=await response.Content.ReadAsStringAsync();
+                Console.WriteLine(json);
+                
+             List< BlogDataModel> lst= JsonConvert.DeserializeObject<List<BlogDataModel>>(json)!;
+                foreach (BlogDataModel model in lst)
+                {
+                    Console.WriteLine(model.BlogTitle);
+                    Console.WriteLine(model.BlogAuthor);
+                    Console.WriteLine(model.BlogContent);
+                    Console.WriteLine(model.DeleteFlag);
+                    Console.WriteLine("-------------------------------------------------------------------");
+                }
+            }
+
+        }
+    }
+}
--- HAMDotNetCore.ConsoleApp/Program.cs
+++ HAMDotNetCore.ConsoleApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿
 using HAMDotNetCore.ConsoleApp;
+using HAMDotNetCore.ConsoleApp.HTTPClientExample;
 using System.Data;
 using System.Data.SqlClient;
 
 
 //AdoDotNetExample example =new AdoDotNetExample();
 //example.Update();
-EFCoreExample ef= new EFCoreExample();
-ef.Read();
+//EFCoreExample ef= new EFCoreExample();
+//ef.Read();
+HttpClientExample http=new HttpClientExample();
+await http.Run();
